f8e26cb7baf18e4a1a8788748a157bbb777c3c17
Johnnei <johnnei07@live.nl>
2016-12-25 12:29:37 +0100

    SGP-3: Improve test failure information.

--- src/test/java/org/johnnei/sgp/CommentOnCommitIT.java
+++ src/test/java/org/johnnei/sgp/CommentOnCommitIT.java
@@ -1,11 +1,12 @@
 package org.johnnei.sgp;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.gitlab.api.models.CommitComment;
-import org.hamcrest.collection.IsCollectionWithSize;
+import org.hamcrest.collection.IsEmptyCollection;
 import org.hamcrest.core.IsCollectionContaining;
 import org.junit.Test;
 
@@ -25,7 +26,6 @@ public class CommentOnCommitIT extends IntegrationTest {
 		sonarAnalysis(commitHash);
 
 		List<CommitComment> commitComments = gitlabApi.getCommitComments(project.getId(), commitHash);
-		assertThat("10 Issues have been reported and thus comments should be there.", commitComments, IsCollectionWithSize.hasSize(10));
 		List<String> comments = commitComments.stream()
 			.map(CommitComment::getNote)
 			.collect(Collectors.toList());
@@ -45,6 +45,23 @@ public class CommentOnCommitIT extends IntegrationTest {
 
 		for (String message : messages) {
 			assertThat(comments, IsCollectionContaining.hasItem(equalTo(message)));
+			remoteMatchedComment(comments, message);
 		}
+
+		assertThat("10 Issues have been reported and thus comments should be there.", comments, IsEmptyCollection.empty());
+	}
+
+	private void remoteMatchedComment(List<String> comments, String message) {
+		// Remove a single matched comment.
+		Iterator<String> commentsIterator = comments.iterator();
+		while (commentsIterator.hasNext()) {
+			String comment = commentsIterator.next();
+			if (comment.equals(message)) {
+				commentsIterator.remove();
+				return;
+			}
+		}
+
+		throw new IllegalStateException("Matcher passed but didn't remove message.");
 	}
 }
