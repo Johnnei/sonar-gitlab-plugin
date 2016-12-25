d1973e30ab5f958701d03f7bcd59ddfb5ff73baa
Johnnei <johnnei07@live.nl>
2016-12-25 14:54:12 +0100

    SGP-3: Add SQ version specific test expectations.

--- src/test/java/org/johnnei/sgp/CommentOnCommitIT.java
+++ src/test/java/org/johnnei/sgp/CommentOnCommitIT.java
@@ -1,6 +1,7 @@
 package org.johnnei.sgp;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -30,25 +31,18 @@ public class CommentOnCommitIT extends IntegrationTest {
 			.map(CommitComment::getNote)
 			.collect(Collectors.toList());
 
-		String[] messages = {
-			"Remove this unused \"inputStream\" local variable.",
-			"Remove this useless assignment to local variable \"inputStream\".",
-			"Close this \"FileInputStream\".",
-			"Either log or rethrow this exception.",
-			"Do not forget to remove this deprecated code someday.",
-			"Replace this \"switch\" statement by \"if\" statements to increase readability.",
-			"Add a default case to this switch.",
-			"Reduce this switch case number of lines from 10 to at most 5, for example by extracting code into methods.",
-			"Refactor this code to not nest more than 3 if/for/while/switch/try statements.",
-			"Change this condition so that it does not always evaluate to \"true\""
-		};
+		List<String> messages = Files.readAllLines(getTestResource("sonarqube/issues.txt"));
 
 		for (String message : messages) {
 			assertThat(comments, IsCollectionContaining.hasItem(equalTo(message)));
 			remoteMatchedComment(comments, message);
 		}
 
-		assertThat("10 Issues have been reported and thus comments should be there.", comments, IsEmptyCollection.empty());
+		assertThat(
+			String.format("%s Issues have been reported and thus comments should be there.", messages.size()),
+			comments,
+			IsEmptyCollection.empty()
+		);
 	}
 
 	private void remoteMatchedComment(List<String> comments, String message) {
